273fb587c124e42bb5af5266b6f91100c56fc4dd
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2026-04-18 14:48:34 +0200

    fix: update comment in app.py for clarity

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import os
 import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-#test workflow again
+#test workflow again and again
 
 def get_runtime_metadata():
     return {
